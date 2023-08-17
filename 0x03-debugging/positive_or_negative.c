c84df22fe20bec21880e5d1c67d579830b2b92cb
Segun Ogundele <talk2banzi@gmail.com>
2023-08-17 11:53:37 -0700

    update on positive negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,17 +1,20 @@
 #include "main.h"
-
+/**
+  *positive_or_negative - tests the value to know positive or negative
+  *@i - integer to check
+  */
 void positive_or_negative(int i)
 {
 	if (i > 0)
 	{
 		printf("%d is positive\n", i);
 	}
-	else if (i == 0)
+	else if (i < 0)
 	{
-		printf("%d is zero\n", i);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", i);
+		printf("%d is zero\n", i);
 	}
 }
