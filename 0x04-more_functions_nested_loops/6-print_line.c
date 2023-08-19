97122133fba9b5fb6caa8a72df862f0956dba045
Segun Ogundele <talk2banzi@gmail.com>
2023-08-19 01:41:41 -0700

    print squares with #

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-  * print_line - draw a line
+  * print_line - straight line
   * @n: numberof times a character is printed
   * @_ : what we print line with
   * Return: always 0
   */
+
 void print_line(int n)
-{
-	int i;
+{	int i;
 
 	for (i = 0; i < n; i++)
 	{
